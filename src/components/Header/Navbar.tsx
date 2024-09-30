f4b71b66c86be0c278798683b08de54f5bd9e931
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2024-09-30 23:52:39 +0600

    navbar complete with mobile and desktop responsive

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Logo from "/src/assets/images/logo.png.webp";
 import Image from "next/image";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { HiMenu, HiX } from "react-icons/hi";
 
 const navData = [
   {
@@ -104,7 +105,10 @@ const Navbar = () => {
                               {child.children.map((subChild, index) => (
                                 <Link
                                   href={subChild.link}
-                                  onMouseEnter={() => setIsSubOpenLevel3(true)}
+                                  onMouseEnter={() =>
+                                    subChild.children &&
+                                    setIsSubOpenLevel3(true)
+                                  }
                                   onMouseLeave={() => setIsSubOpenLevel3(false)}
                                   key={index}
                                   className="space-y-2 py-3 px-4 hover:bg-primary block">
@@ -152,34 +156,103 @@ const Navbar = () => {
             onClick={toggleMenu}
             className="md:hidden text-white focus:outline-none">
             <div className="space-y-1">
-              <span className="block w-6 h-0.5 bg-white"></span>
-              <span className="block w-6 h-0.5 bg-white"></span>
-              <span className="block w-6 h-0.5 bg-white"></span>
+              {isOpen ? (
+                <HiX className="text-2xl" />
+              ) : (
+                <HiMenu className="text-2xl" />
+              )}
             </div>
           </button>
         </div>
 
         {/* Mobile Menu */}
         {isOpen && (
-          <div className="md:hidden mt-4 space-y-4">
-            <Link
-              href="/solutions"
-              className="block text-white hover:text-orange-400">
-              Solutions
-            </Link>
-            <Link
-              href="/blog"
-              className="block text-white hover:text-orange-400">
-              Blog
-            </Link>
-            <Link
-              href="/contact"
-              className="block text-white hover:text-orange-400">
-              Contact
-            </Link>
+          <div className="md:hidden mt-1 space-y-6 bg-[#1A1A1A] text-center py-3">
+            {navData.map((item, index) => (
+              <div className="relative group">
+                <Link
+                  key={index}
+                  href={item.link}
+                  className="block text-white uppercase font-bold hover:bg-primary py-3">
+                  {item.title}
+                </Link>
+                {item.children && (
+                  <div className="relative bg-tertiary hidden group-hover:block w-full">
+                    {item.children.map((child, index) => (
+                      <>
+                        <Link
+                          onMouseOver={() =>
+                            item.children && setIsSubOpenLevel2(true)
+                          }
+                          onMouseLeave={() => setIsSubOpenLevel2(false)}
+                          key={index}
+                          href={child.link}
+                          className="block text-white  text-sm">
+                          <span
+                            className={`w-full grid grid-cols-3 justify-between items-center hover:bg-primary py-3 px-4 ${
+                              isSubOpenLevel2 ? "bg-primary" : null
+                            }`}>
+                            <span></span>
+                            {child.title}
+                            {child.children && (
+                              <FaArrowRightLong className="mr-[32px] place-self-end" />
+                            )}
+                          </span>
+                          {child.children && isSubOpenLevel2 && (
+                            <div className="bg-gray-500 container hidden group-hover:block w-full">
+                              {child.children.map((subChild, index) => (
+                                <>
+                                  <Link
+                                    onMouseOver={() =>
+                                      child.children && setIsSubOpenLevel3(true)
+                                    }
+                                    onMouseLeave={() =>
+                                      setIsSubOpenLevel3(false)
+                                    }
+                                    href={subChild.link}
+                                    key={index}
+                                    className="block text-sm">
+                                    <span
+                                      className={`flex justify-between items-center hover:bg-primary py-3 px-4 ${
+                                        subChild.children && isSubOpenLevel3
+                                          ? "bg-primary"
+                                          : null
+                                      }`}>
+                                      {subChild.title}
+                                      {subChild.children && (
+                                        <FaArrowRightLong className="" />
+                                      )}
+                                    </span>
+                                    {subChild.children && isSubOpenLevel3 && (
+                                      <div className="bg-gray-500 container hidden group-hover:block w-full">
+                                        {subChild.children.map(
+                                          (subSubChild, index) => (
+                                            <Link
+                                              href={subSubChild.link}
+                                              key={index}
+                                              className="space-y-2 py-3 px-4 hover:bg-primary block text-sm text-start">
+                                              {subSubChild.title}
+                                            </Link>
+                                          )
+                                        )}
+                                      </div>
+                                    )}
+                                  </Link>
+                                </>
+                              ))}
+                            </div>
+                          )}
+                        </Link>
+                      </>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+
             <Link
               href="/tools"
-              className="block text-white hover:text-orange-400 border-2 border-orange-500 px-3 py-1 rounded-lg">
+              className="block text-white border-[3px] border-primary px-3 py-2 rounded-full uppercase font-bold">
               Tools
             </Link>
           </div>
